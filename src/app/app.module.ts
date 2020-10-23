64d8f4ef3ff7969bd6205eeae7c7f596f4d4ac7b
mittalDesign <shashank.mittal@westernacher.com>
2020-10-23 21:01:11 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConvasComponent } from './convas/convas.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutComponent } from './about/about.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConvasComponent,
+    NavigationComponent,
+    LandingPageComponent,
+    IntroComponent,
+    AboutComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
